3ec724ceaba8bb53f7e7e4e2fe94d9c73b711a5b
usama <42535017+Usamaraoo@users.noreply.github.com>
2020-09-05 16:37:19 +0500

    delete apki key

--- app/views.py
+++ app/views.py
@@ -4,22 +4,32 @@ from urllib.parse import urlencode
 
 
 def map(request):
+
     if request.method == "POST":
         area = request.POST.get('area')
         print(area)
-        lat_long = (extract_lat_lng(area))
-        lat = (lat_long[0])
-        lng = (lat_long[1])
 
-    context = {'lat': lat, 'lng': lng}
+    context = {
+        'lat': -25.344,
+        'lng': 131.036,
+    }
+
     return render(request, 'map.html', context)
 
 
-# GeoCoding Api
-api_key = 'AIzaSyCI8hoW9BplzJXyxgiiCdZgxnuPHsFezLA'
+api_key = 'key'
+data_type = 'json'
+endpoint = f"https://maps.googleapis.com/maps/api/geocode/{data_type}"
+params = {"address": "1600 Amphitheatre Parkway, Mountain View, CA", "key": api_key}
+url_params = urlencode(params)
+
+url = f"{endpoint}?{url_params}"
+print(url)
+api_key = 'provide key'
 
 
 def extract_lat_lng(address_or_postalcode, data_type='json'):
+
     endpoint = f"https://maps.googleapis.com/maps/api/geocode/{data_type}"
     params = {"address": address_or_postalcode, "key": api_key}
     url_params = urlencode(params)
@@ -33,3 +43,6 @@ def extract_lat_lng(address_or_postalcode, data_type='json'):
     except:
         pass
     return latlng.get("lat"), latlng.get("lng")
+
+
+print(extract_lat_lng("karachi"))
